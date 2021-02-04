97b96af6c4774fa6d70480d13f8620676b3c0b0d
mronzetti <ronzettimh@nih.gov>
2021-02-04 06:30:21 -0500

    Coded iteration loop to handle files with queries larger than NCBI likes.

--- seq_retrieve.R
+++ seq_retrieve.R
@@ -3,13 +3,24 @@ library(rentrez)
 library(readxl)
 library(tidyverse)
 
-## make a character vector of accession IDs
-prot_ids <- read_xlsx('./merops_seqs.xlsx', sheet='all')
-df_id <- as.vector(prot_ids$NCBI_ID)
+## Pull in file containing MEROPS-copied accession IDS and remove empty rows
+prot_ids <- read.csv('./data/merops_seqs.csv')
+prot_ids <- prot_ids %>%
+  select('NCBI_ID') %>%
+  na_if("") %>%
+  na.omit()
 
 ## use entrez_fetch() to get the protein FASTAs
 ## set `db = "protein"` to specify the database type as protein
-prot_fasta <- entrez_fetch(id = df_id,
-                           db = 'protein',
-                           rettype = 'fasta')
-write(prot_fasta, file="my_proteins.fasta")
+## You may need to un-comment the sleep box in order to access NCBI resources
+full_fasta <- c('')
+for(i in 1:length(prot_ids$NCBI_ID)){
+  temp_id <- prot_ids$NCBI_ID[(i)]
+  temp_fasta <- entrez_fetch(id = temp_id,
+                             db = 'protein',
+                             rettype = 'fasta',
+                             api_key='9d27de55176f553529fb57ad6d2217bf3f08')
+  full_fasta[(i)] <- temp_fasta
+  #sleep(0.1)
+}
+write(full_fasta, file="my_proteins.fasta")
